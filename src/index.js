5374fb2c9581e30395aadaa8f8c66a92c85fedc6
Kong310544 <kong310544@gmail.com>
2022-02-01 17:08:09 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use("/user",useRoute);
 
 // Routing Table
 app.get("/",(req, res)=>{
-    res.send("Hello from index");
+    res.send("Hello from index 5555");
 });
 
 app.get("/login",(req, res)=>{
